951c35222cc4feb6a8385624c250872f8a7b4211
Haden Moore <hadens.moore@gmail.com>
2020-05-19 10:36:37 -0500

    Completed max_min.py

---
+++ Course_2/Assign5.2/max_min.py
@@ -0,0 +1,33 @@
+"""5.2 Write a program that repeatedly prompts a user for integer
+numbers until the user enters 'done'. Once 'done' is entered, print
+out the largest and smallest of the numbers. If the user enters
+anything other than a valid number catch it with a try/except and
+put out an appropriate message and ignore the number. Enter 7, 2,
+bob, 10, and 4 and match the output below."""
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number:\n")
+    
+    if num == "done": break
+    if len(num) == 0: break
+
+    try:
+    	num = int(num)
+    	if smallest is None or smallest > num:
+    		smallest = num;
+    		
+    	if largest is None or largest < num:
+    		largest = num;
+
+    except Exception:
+    	print ("Invalid input")
+
+print ("Maximum is", largest)
+print ("Minimum is", smallest)
+
+# # Desired OutPut = 
+# Invalid input
+# Maximum is 10 
+# Minimum is 2
