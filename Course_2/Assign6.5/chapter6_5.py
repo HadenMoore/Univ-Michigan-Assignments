011a22e4f712dc8f52600b28237951cc159443e4
Haden Moore <hadens.moore@gmail.com>
2020-05-19 10:42:34 -0500

    Updating with The Assignment Quesiton

--- Course_2/Assign6.5/chapter6_5.py
+++ Course_2/Assign6.5/chapter6_5.py
@@ -1,3 +1,8 @@
+"""6.5 Write code using find() and string slicing (see section 6.10) to extract the
+number at the end of the line below.
+Convert the extracted value to a floating point number and print it out."""
+
+
 str = "X-DSPAM-Confidence: 0.8475"
 
 ipos = str.find(':')
@@ -8,3 +13,5 @@ piece = str[ipos+2:]
 value = float(piece)
 print(value)
 # print(value+42.0)
+
+# Desired Output = 0.8475
