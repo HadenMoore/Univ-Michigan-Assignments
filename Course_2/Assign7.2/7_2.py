d52f0146be7265d7498d66add9e4b46a4f0ee893
Haden Moore <hadens.moore@gmail.com>
2020-05-19 14:16:35 -0500

    Update 7_2.py

---
+++ Course_2/Assign7.2/7_2.py
@@ -0,0 +1,27 @@
+"""7.2 Write a program that prompts for a file name, then opens that file and reads through the file, 
+looking for lines of the form: X-DSPAM-Confidence:    0.8475
+Count these lines and extract the floating point values from each of the lines and
+compute the average of those values and produce an output as shown below.
+not use the sum() function or a variable named sum in your solution.
+You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+when you are testing below enter mbox-short.txt as the file name."""
+
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    print(line)
+print("Done")
+
+
+
+
+
+
+
+
+
+
+# Desired Output
+# Average spam confidence: 0.750718518519
