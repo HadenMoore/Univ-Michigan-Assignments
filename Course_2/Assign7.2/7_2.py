857c649c81836c66b549f6d83018f2af201f715e
Haden Moore <hadens.moore@gmail.com>
2020-05-19 15:30:40 -0500

    Assignment 7.2 Completed.

--- Course_2/Assign7.2/7_2.py
+++ Course_2/Assign7.2/7_2.py
@@ -9,19 +9,19 @@ when you are testing below enter mbox-short.txt as the file name."""
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+count = 0
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
-    print(line)
-print("Done")
 
+    line = line.rstrip()
+    number =float((line[19:26]))
 
+    count = count + 1
+    average = average + number
 
-
-
-
-
-
-
+average = (average / count)
+average = float(average)
+print("Average spam confidence:",average)
 
 # Desired Output
 # Average spam confidence: 0.750718518519
