96371a89e1309a464dd79cde94953626d6dedbcd
Haden Moore <hadens.moore@gmail.com>
2020-06-01 13:18:47 -0500

    Assign12.1 Completed.

--- Course_3/Assign12.1/socket1.py
+++ Course_3/Assign12.1/socket1.py
@@ -11,3 +11,17 @@ Use the telnet program as shown in lecture to retrieve the headers and content.
 Enter the header values in each of the fields below and press "Submit"."""
 
 
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
