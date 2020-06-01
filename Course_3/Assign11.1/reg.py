e9d3342a2d8de101b023ebb8b76104a9c0034340
Haden Moore <hadens.moore@gmail.com>
2020-06-01 11:34:51 -0500

    Update for PY4E Assignment/ Complete.

--- Course_3/Assign11.1/reg.py
+++ Course_3/Assign11.1/reg.py
@@ -15,10 +15,16 @@ someone else solve a problem.  This book assumes that
 everyone needs to know how to program ...
 
 This is a sample program and I do not guarantee it is the solution to the actual assignment, as this differs often.
-I have included a sample file to test the program."""
+I have included a sample file to test the program.
+
+Sample data: http://py4e-data.dr-chuck.net/regex_sum_42.txt
+(There are 90 values with a sum=445833)
+
+Actual data: http://py4e-data.dr-chuck.net/regex_sum_555672.txt
+(There are 74 values and the sum ends with 497)"""
 
 
-import re
+"""import re
 count = 0
 handle = open("test_555672.txt")
 lines = handle.read()
@@ -27,8 +33,26 @@ for i in match:
 	number = int(i)
 	count = count + number
 print(count)
-
+"""
 
 #Sample File Sum After Running = 445833
 
 # Test File Sum After Running = 316497
+
+
+""" PY4E Assignment 11.1 has a Different Test File with A dfferent Sum
+Sample data: http://py4e-data.dr-chuck.net/regex_sum_42.txt
+(There are 90 values with a sum=445833)
+
+Actual data: http://py4e-data.dr-chuck.net/regex_sum_555452.txt
+(There are 89 values and the sum ends with 604) """ 
+
+import re
+count = 0
+handle = open("test_555452.txt")
+lines = handle.read()
+match = re.findall('[0-9]+',lines)
+for i in match:
+	number = int(i)
+	count = count + number
+print(count)
