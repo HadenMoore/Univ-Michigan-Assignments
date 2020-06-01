55a8085be8d93e427379022fa220ce5c987289e5
Haden Moore <hadens.moore@gmail.com>
2020-06-01 13:28:43 -0500

    Adding files.

--- Course_3/Assign12.2/bs4.py
+++ Course_3/Assign12.2/bs4.py
@@ -24,7 +24,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = input('Enter - http://py4e-data.dr-chuck.net/comments_42.html')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
