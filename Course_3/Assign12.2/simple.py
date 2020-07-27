a49f2c290ad87b515c1d073f31cf21f322957444
Haden Moore <hadens.moore@gmail.com>
2020-07-27 12:21:13 -0500

    Assignment Complete

--- Course_3/Assign12.2/simple.py
+++ Course_3/Assign12.2/simple.py
@@ -10,7 +10,7 @@ Sample Data: http://py4e-data.dr-chuck.net/comments_42.html (Sum = 2553)
 Actual Data:  http://py4e-data.dr-chuck.net/comments_555454.html (Sum Ends with 95) """
 
 import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
+from bs4 import *
 import ssl
 
 # Ignore SSL Certificate Errors
@@ -18,10 +18,29 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
+"""Acutal Data link for Coursera AutoGrader: http://py4e-data.dr-chuck.net/comments_555674.html (Sum Ends with 35)
+url = 'http://py4e-data.dr-chuck.net/comments_555674.html'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, "html.parser")"""
+
+#Acutal Data link for PY4E AutoGrader: http://py4e-data.dr-chuck.net/comments_555454.html (Sum Ends with 95)
+url = 'http://py4e-data.dr-chuck.net/comments_555674.html'
+html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
 
+# Retrieve All <span> Tags: 
+spans = soup('span')
+
+# Calculate a Sum of All Numbers in Span Tags: 
+sum = 0
+for span in spans: 
+    sum = sum + int(span.contents[0])
+print(sum)
+
+
+
+
+"""
 # To Retrieve all of the Anchor Tags: 
 tags = soup('a')
 for tag in tags:
@@ -29,10 +48,4 @@ for tag in tags:
     print('URL:', tag.get('href', None))
     print('Contents:', tag.contents[0])
     print('Attrs:', tag.attrs)
-
-
-
-
-
-# # To Retrieve all <span> Tags: 
-# tags = soup('')
+"""
