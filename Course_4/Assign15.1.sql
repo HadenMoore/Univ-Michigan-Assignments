f9fc0bdaac596e69c6c7ad126d44ebe56779fb13
Haden Moore <hadens.moore@gmail.com>
2020-07-28 12:35:47 -0500

    Update

--- Course_4/Assign15.1.sql
+++ Course_4/Assign15.1.sql
@@ -1,6 +1,1 @@
-DELETE FROM Ages; 
-INSERT INTO Ages (name, age) VALUES ( 'Alecia', 21); 
-INSERT INTO Ages (name, age) VALUES ('Lileidh', 29);
-INSERT INTO Ages (name, age) VALUES ('Tailee', 13);
-INSERT INTO Ages (name, age) VALUES ('Aanya', 17);
-INSERT INTO Ages (name, age) VALUES ('Joaquin', 15);
+SELECT hex(name || age) AS X FROM Ages ORDER BY X
