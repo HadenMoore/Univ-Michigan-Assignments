113ae8599cb5ce07c981af5b3658cf5aced21fa7
Haden Moore <hadens.moore@gmail.com>
2020-07-28 12:33:05 -0500

    Created Data Base

---
+++ Course_4/Assign15.1.sql
@@ -0,0 +1,6 @@
+DELETE FROM Ages; 
+INSERT INTO Ages (name, age) VALUES ( 'Alecia', 21); 
+INSERT INTO Ages (name, age) VALUES ('Lileidh', 29);
+INSERT INTO Ages (name, age) VALUES ('Tailee', 13);
+INSERT INTO Ages (name, age) VALUES ('Aanya', 17);
+INSERT INTO Ages (name, age) VALUES ('Joaquin', 15);
