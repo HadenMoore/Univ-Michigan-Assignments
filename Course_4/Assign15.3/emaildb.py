988fbf74ca2368f75ffb5ec606bc726a41010506
Haden Moore <hadens.moore@gmail.com>
2020-08-10 12:06:01 -0500

    Updating

--- Course_4/Assign15.3/emaildb.py
+++ Course_4/Assign15.3/emaildb.py
@@ -15,18 +15,18 @@ for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1] 
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
     row = cur.fetchone()
     if row is None: 
-        cur.execute('''INSERT INTO Counts (email, count)
-            VALUES (?, 1)''', (email,))
+        cur.execute('''INSERT INTO Counts (org, count)
+            VALUES (?, 1)''', (org,))
     else: 
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
-            (email,))
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+            (org,))
     conn.commit()
 
 # https://sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
