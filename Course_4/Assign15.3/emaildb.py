b749bbc09c3359436a944893f22d7bf7123face7
Haden Moore <hadens.moore@gmail.com>
2020-08-10 13:41:40 -0500

    Assign15.3 Complete.

--- Course_4/Assign15.3/emaildb.py
+++ Course_4/Assign15.3/emaildb.py
@@ -9,12 +9,13 @@ cur.execute('''
 CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = input('Enter File Name: ')
-if (len(fname) < 1): fname = 'mbox-short.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
-    org = pieces[1] 
+    db = pieces[1].split('@')
+    org = db[1]
     cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
     row = cur.fetchone()
     if row is None: 
